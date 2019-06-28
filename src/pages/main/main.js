77557e892b4d378fc40650450a29b09e02ec298d
qinglingzhiyu <1012173543@qq.com>
2019-06-28 10:54:05 +0800

    init

---
+++ src/pages/main/main.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: terris
+ * @Github: https://github.com/qinglingzhiyu
+ * @Date: 2019-06-28 09:28:47
+ * @LastEditors: terris
+ * @LastEditTime: 2019-06-28 10:42:56
+ * @Description: 这是首页页面&逻辑
+ */
