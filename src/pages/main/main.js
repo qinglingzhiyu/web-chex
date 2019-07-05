327ddfac250464b46ac9c2b40e061904baffa027
qinglingzhiyu <1012173543@qq.com>
2019-07-05 19:11:15 +0800

    日常提交

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -3,17 +3,24 @@
  * @Github: https://github.com/qinglingzhiyu
  * @Date: 2019-07-02 10:10:15
  * @LastEditors: terris
- * @LastEditTime: 2019-07-04 17:14:54
+ * @LastEditTime: 2019-07-05 19:10:10
  * @Description: 首页
  */
 
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text, Image } from '@tarojs/components'
+import { View, Text, Image, Swiper, SwiperItem } from '@tarojs/components'
 import TopBanner from './topBanner'
 import CarBanner from './carBanner'
 import location from './assets/location.png'
 import message from './assets/message.png'
 import search from './assets/search.png'
+import washingImg from './assets/1001.png'
+import maintainImg from './assets/1002.png'
+import tireImg from './assets/1003.png'
+import rescueImg from './assets/1004.png'
+import signImg from './assets/1005.png'
+import redpackImg from './assets/redpack.png'
+import handpick from './assets/handpick.png'
 import './main.scss'
 
 class Main extends Component {
@@ -22,6 +29,28 @@ class Main extends Component {
   }
 
   state = {
+    navList:[
+      {
+        icon: washingImg,
+        title: '洗车服务'
+      },
+      {
+        icon: maintainImg,
+        title: '大小保养'
+      },
+      {
+        icon: tireImg,
+        title: '换轮胎'
+      },
+      {
+        icon: rescueImg,
+        title: '道路救援'
+      },
+      {
+        icon: signImg,
+        title: '每日签到'
+      },
+    ]
   }
 
   componentWillMount() {}
@@ -35,6 +64,7 @@ class Main extends Component {
   componentDidHide() {}
 
   render() {
+    const { navList } = this.state
     return (
       <View className='main'>
         <View className='main_search'>
@@ -46,8 +76,32 @@ class Main extends Component {
           </View>
           <Image className='main_search-message' src={message} />
         </View>
-        <TopBanner></TopBanner>
-        <CarBanner></CarBanner>
+        <View className='main-topbanner'>
+          <TopBanner></TopBanner>
+        </View>
+        <View className='main-carbanner'>
+          <CarBanner></CarBanner>
+        </View>
+        <View className='main-nav'>
+          {navList.map( (nav,index)=>{
+            return (
+              <View key={index} className='main-nav-item'>
+                <Image className='main-nav-item-img' src={nav.icon} />
+                <Text className='main-nav-item-text'>{nav.title}</Text>
+              </View>
+            )})}
+        </View>
+        <View className='main-redpack'>
+          <Image className='main-redpack-img' src={redpackImg} />
+        </View>
+        <View className='main-handpick'>
+          <Image className='main-handpick-img' src={handpick} />
+          <Swiper className='main-handpick-swiper'>
+            <SwiperItem className='main-handpick-swiper-item'>
+              一车独秀APP荣登store下载我们荣尽可
+            </SwiperItem>
+          </Swiper>
+        </View>
       </View>
     )
   }
